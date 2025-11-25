adf120b28001e595c06b0b7396d72396d44e6622
Kuredew <burrohmanhabib35@gmail.com>
2025-11-25 22:22:29 +0700

    feat(view/dashboard): Add kick button for leader

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -158,7 +158,19 @@
                                                 <p class="text-xs text-muted-foreground">Member</p>
                                             </div>
                                         </div>
+                                        <div class="flex gap-5">
+                                        @if ($squad->leader_id == $student->id)
+                                        <form action="{{ route('squads.kick', $student) }}" method="post">
+                                            @csrf
+                                            <button>
+                                                <svg onclick="confirm('Apakah kamu yakin ingin mengeluarkan {{ $student->name }}?');" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 opacity-50 hover:opacity-100 transition-all cursor-pointer">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
+                                                </svg>
+                                            </button>
+                                        </form>
+                                        @endif
                                         <div class="inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-primary/30 text-primary">Member</div>
+                                        </div>
                                                 @endif
                                     </div>
                                     @endforeach
