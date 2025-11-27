80d35b71af4ec0661be05f3962b684418dd68fc1
ry726 <riyanxd726@gmail.com>
2025-11-27 19:25:34 +0700

    feat (bootstrap) : Add teacher routes and teacher.auth middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         web: [
             __DIR__.'/../routes/web.php',
             __DIR__.'/../routes/api.php',
+            __DIR__.'/../routes/teacher.php',
         ],
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
@@ -20,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'guest' => GuestMiddleware::class,
             'student.auth' => StudentAuthMiddleware::class,
+            'teacher.auth' => \App\Http\Middleware\TeacherAuthMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
