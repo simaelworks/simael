32b9b112fa9b5c23896292e3df7ae0473d27dc01
Caxerion <kennethgeveny1@gmail.com>
2025-11-28 18:24:47 +0700

    [dashboard] fix some function issues

--- app/Http/Controllers/SquadController.php
+++ app/Http/Controllers/SquadController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Squad;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SquadController extends Controller
 {
