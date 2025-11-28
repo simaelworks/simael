3390491f77af9f59568b95abcbd606dcd55e5111
Kuredew <burrohmanhabib35@gmail.com>
2025-11-28 11:25:47 +0700

    refactor(seeder): Remove inviteSquad seeder in call list
    Because this is unused for development

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,7 +27,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DefaultSquadSeeder::class,
             DefaultStudentSeeder::class,
-            DefaultInviteSquadSeeder::class,
             DefaultTeacherSeeder::class,
         ]);
     }
