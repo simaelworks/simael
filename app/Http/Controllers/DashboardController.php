14448c1a2796826b8fb2810ac3f6b0896cb5fc83
Kuredew <burrohmanhabib35@gmail.com>
2025-11-28 15:40:19 +0700

    fix(controller/dashboard): Find authenticate user with Auth facades

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     //
     public function index()
     {
-        $student = Student::find(session('student_id'));
+        $student = Auth::guard('student')->user();
         $squad = $student->squad;
 
         // If status is pending, route to pending page
