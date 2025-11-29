1d70ec65fb8a1484ec71b7540286e15123b021f1
ry726 <riyanxd726@gmail.com>
2025-11-30 01:23:06 +0700

    refactor(dashboard) : Add alert for kicking member

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -160,10 +160,10 @@
                                         </div>
                                         <div class="flex gap-5">
                                         @if ($squad->leader_id == $student->id)
-                                        <form action="{{ route('squads.kick', $member) }}" method="post">
+                                        <form action="{{ route('squads.kick', $member) }}" method="post" onsubmit="return confirm('Apakah kamu yakin ingin mengeluarkan {{ $member->name }} dari Squad?');">
                                             @csrf
-                                            <button>
-                                                <svg onclick="confirm('Apakah kamu yakin ingin mengeluarkan {{ $member->name }}?');" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 opacity-50 hover:opacity-100 transition-all cursor-pointer">
+                                            <button type="submit">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 opacity-50 hover:opacity-100 transition-all cursor-pointer">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
                                                 </svg>
                                             </button>
