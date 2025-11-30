b57de219b247e7bdd4e3b2bac6ff3e738b68323e
ry726 <riyanxd726@gmail.com>
2025-11-30 12:18:13 +0700

    refactor(api) : Teacher API endpoints for searching students and squads

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\InviteSquad;
 use App\Models\Student;
+use App\Models\Squad;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -29,5 +30,39 @@ class ApiController extends Controller
         InviteSquad::create($validate);
 
         return response('Successfully make invite request to Student', 200);
+    }
+
+    // Teacher API endpoints for searching students
+    public function teacherSearchStudents(Request $request)
+    {
+        $search = $request->query('search', '');
+        
+        if (empty($search)) {
+            return response()->json([]);
+        }
+
+        $students = Student::where('name', 'like', "%{$search}%")
+            ->orWhere('nisn', 'like', "%{$search}%")
+            ->orWhere('id', 'like', "%{$search}%")
+            ->with('squad')
+            ->get();
+
+        return response()->json($students);
+    }
+
+    // Teacher API endpoints for searching squads
+    public function teacherSearchSquads(Request $request)
+    {
+        $search = $request->query('search', '');
+        
+        if (empty($search)) {
+            return response()->json([]);
+        }
+
+        $squads = Squad::where('name', 'like', "%{$search}%")
+            ->with(['leader', 'users'])
+            ->get();
+
+        return response()->json($squads);
     }
 }
